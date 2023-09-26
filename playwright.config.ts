f1dd094d208bb50944c8fd3c0a12bab6f6707872
Paweł Wojciechowski <112585950+Pawel-Albert@users.noreply.github.com>
2023-09-26 20:36:10 +0200

    Update playwright.config.ts - screenshot: 'only-on-failure'

--- playwright.config.ts
+++ playwright.config.ts
@@ -22,6 +22,7 @@ export default defineConfig({
   },
   /* Run tests in files in parallel */
   fullyParallel: true,
+  screenshot: 'only-on-failure',
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
